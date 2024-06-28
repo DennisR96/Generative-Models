61c951288fee75685a4341ff68a97e3658f8dfd4
Dennis Räk <dennisraek@gmail.com>
2024-06-29 00:25:51 +0200

    add detected device

--- models/base.py
+++ models/base.py
@@ -17,10 +17,13 @@ class Base():
         Check Available Device
         """
         if torch.cuda.is_available():
+            print("-- CUDA Device detected --")
             device = "cuda"
         elif torch.backends.mps.is_available():
+            print("-- MPS Device detected --")
             device = "mps"
         else:
+            print("-- CPU Device detected --")
             device = "cpu"
         return device
     
